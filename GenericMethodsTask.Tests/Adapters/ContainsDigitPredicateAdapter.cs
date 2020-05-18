be4ea648ace34fe8b296a489f602696b87c77b12
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-18 13:12:40 +0000

    add-predicate-adapter

---
+++ GenericMethodsTask.Tests/Adapters/ContainsDigitPredicateAdapter.cs
@@ -0,0 +1,14 @@
+﻿using GenericMethodsTask.Interfaces;
+
+namespace GenericMethodsTask.Tests.Adapters
+{
+    internal class ContainsDigitPredicateAdapter : IPredicate<int>
+    {
+        //TODO: Add necessary code and remove this comment.
+        
+        public bool Verify(int obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
