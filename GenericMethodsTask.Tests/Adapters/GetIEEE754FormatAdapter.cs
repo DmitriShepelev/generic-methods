0129c12c851411494756f452b54fddafc2bf37c0
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-18 13:13:03 +0000

    add-transformer-adapter

---
+++ GenericMethodsTask.Tests/Adapters/GetIEEE754FormatAdapter.cs
@@ -0,0 +1,14 @@
+﻿using GenericMethodsTask.Interfaces;
+
+namespace GenericMethodsTask.Tests.Adapters
+{
+    internal class GetIEEE754FormatAdapter : ITransformer<double, string>
+    {
+        //TODO: Add necessary code and remove this comment.
+        
+        public string Transform(double obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
