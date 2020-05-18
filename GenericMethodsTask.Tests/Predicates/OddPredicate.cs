a3fbce062075c7638d4b97becb97ceaed7c10332
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-18 13:25:37 +0000

    Update OddPredicate.cs

--- GenericMethodsTask.Tests/Predicates/OddPredicate.cs
+++ GenericMethodsTask.Tests/Predicates/OddPredicate.cs
@@ -2,7 +2,7 @@ using GenericMethodsTask.Interfaces;
 
 namespace GenericMethodsTask.Tests.Predicates
 {
-    internal class OddPredicate : IPredicate<int>
+    public class OddPredicate : IPredicate<int>
     {
         public bool Verify(int value) => value % 2 != 0;
     }
