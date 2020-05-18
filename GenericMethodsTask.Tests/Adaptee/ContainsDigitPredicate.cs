07d1575e7637d1d31f0b3ea7b2cababadf2efea3
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-18 13:10:25 +0000

    add-predicate-adaptee

---
+++ GenericMethodsTask.Tests/Adaptee/ContainsDigitPredicate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericMethodsTask.Tests.Adaptee
+{
+    //Note: This class should not implement any interfaces
+    //Put your code from class ContainsDigitPredicate see 
+    // https://gitlab.com/epam-autocode-tasks/filter-strategy/-/blob/master/FilterStrategyTask.Tests/PredicateImplementations/ContainsDigitPredicate.cs
+    internal class ContainsDigitPredicate
+    {
+        //TODO: Add code here if necessary or delete this comment.
+        
+        public int Digit
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        
+        public bool Verify(int value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
