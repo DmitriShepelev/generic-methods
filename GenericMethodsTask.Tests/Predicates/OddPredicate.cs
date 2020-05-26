e402d5acac7b36453cadb426bb6bcfda19411add
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-26 14:00:08 +0000

    Delete OddPredicate.cs

--- GenericMethodsTask.Tests/Predicates/OddPredicate.cs
+++
@@ -1,9 +0,0 @@
-using GenericMethodsTask.Interfaces;
-
-namespace GenericMethodsTask.Tests.Predicates
-{
-    public class OddPredicate : IPredicate<int>
-    {
-        public bool Verify(int value) => value % 2 != 0;
-    }
-}
