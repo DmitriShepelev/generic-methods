bc6f0291be7fe0f22f313242a8ea9165cf7ca6ff
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-26 13:54:15 +0000

    add-adapter-class

---
+++ GenericMethodsTask.Tests/Adapters/ContainsDigitPredicateAdapter.cs
@@ -0,0 +1,18 @@
+﻿using ContainsDigitPredicate;
+using GenericMethodsTask.Interfaces;
+
+namespace GenericMethodsTask.Tests.Adapters
+{
+    public class ContainsDigitContainsDigitValidatorAdapter : IPredicate<int>
+    {
+        //TODO: Add necessary code and remove this comment.
+        public ContainsDigitContainsDigitValidatorAdapter(ContainsDigitValidator validator)
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool Verify(int obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
