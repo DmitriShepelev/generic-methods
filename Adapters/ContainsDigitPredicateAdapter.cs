3fa127f8cd8e74070a5069d6d4437dffdcc5b9a6
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-31 18:51:33 +0000

    Update ContainsDigitPredicateAdapter.cs

--- Adapters/ContainsDigitPredicateAdapter.cs
+++ Adapters/ContainsDigitPredicateAdapter.cs
@@ -1,15 +1,15 @@
-﻿using ContainsDigitPredicate;
+using ContainsDigitPredicate;
 using GenericMethodsTask.Interfaces;
 
 namespace Adapters
 {
     public class ContainsDigitContainsDigitValidatorAdapter : IPredicate<int>
     {
-        //TODO: Add necessary code and remove this comment.
         public ContainsDigitContainsDigitValidatorAdapter(ContainsDigitValidator validator)
         {
             throw new System.NotImplementedException();
         }
+        
         public bool Verify(int obj)
         {
             throw new System.NotImplementedException();
