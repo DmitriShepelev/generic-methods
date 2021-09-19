a8dfa4054096bfd4349aa38defc4a76fb7e6ff2a
dmitri_shepelev <shepelev.dm@gmail.com>
2021-09-20 00:09:40 +0300

    Update Digit Validator

---
+++ ContainsDigitPredicate/ContainsDigitValidator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ContainsDigitPredicate
+{
+    public class ContainsDigitValidator
+    {
+        private int digit;
+
+        public int Digit
+        {
+            get => this.digit;
+            set
+            {
+                if (value >= 0 && value <= 9)
+                {
+                    this.digit = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"Expected digit can not be less than zero and more than nine.");
+                }
+            }
+        }
+
+        public bool Verify(int number)
+        {
+            if (number == this.digit)
+            {
+                return true;
+            }
+
+            while (number != 0)
+            {
+                if (Math.Abs(number % 10) == this.digit)
+                {
+                    return true;
+                }
+
+                number /= 10;
+            }
+
+            return false;
+        }
+    }
+}
